805f5f5cd740c4fdeaa9b7088110d55d9cca0ba9
Priyadarshan Kelkar <priyadarshan.kelkar@optus.com.au>
2017-08-21 15:04:00 +1000

    updated

--- src/main/java/au/com/qantas/example/crawler/service/UrlTrackingService.java
+++ src/main/java/au/com/qantas/example/crawler/service/UrlTrackingService.java
@@ -5,7 +5,9 @@ package au.com.qantas.example.crawler.service;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
@@ -14,6 +16,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import au.com.qantas.example.crawler.model.PageNode;
 
@@ -26,8 +31,7 @@ public class UrlTrackingService implements UrlTrackingServiceIF {
 
 	private Set<PageNode> urlTree = new HashSet<>();
 	private Set<String> visitedUrls = new HashSet<>();
-	
-
+	private Set<PageNode> childTree = new HashSet<>();
 
 	/*
 	 * (non-Javadoc)
@@ -38,35 +42,51 @@ public class UrlTrackingService implements UrlTrackingServiceIF {
 	@Override
 	public Set<PageNode> buildUrlPageTree(String url, int depth) {
 		
-		
+		PageNode pageNode = buildPageNode(url,depth);
+		Set<PageNode> pageNodes = pageNode.getNodes();
+		if(!CollectionUtils.isEmpty(pageNodes)){
+			for(PageNode node:pageNodes){
+				String nodeUrl = node.getUrl();
+				PageNode childNode = buildPageNode(nodeUrl,depth);
+				childTree.add(childNode);
+			}
+		}
+		pageNode.setNodes(childTree);
+		urlTree.add(pageNode);
+		return urlTree;
+	}
+	
+	private PageNode buildPageNode(String url, int depth){
+		PageNode parentNode = new PageNode();
 		try {
-			PageNode parentNode = new PageNode();
 			
-			if(!isUrlVisited(url) && StringUtils.isNotBlank(url)){
+			if(StringUtils.isNotBlank(url)){
 				visitedUrls.add(url);
 				Document parentDoc = Jsoup.connect(url).get();			
 				parentNode.setUrl(url);
 				parentNode.setTitle(parentDoc.title());
 				Elements webLinks = parentDoc.select("a[href]");
+				List<Element> elementList = webLinks.stream()
+													.limit(depth)
+													.collect(Collectors.toList());
+				webLinks = new Elements(elementList);
 				Set<PageNode> childNodes = new HashSet<>();
-				for(Element webPage: webLinks){
+				for (Element webPage : webLinks) {
 					String absUrl = webPage.absUrl("abs:href");
-//					System.out.println("webPage.absUrl: "+absUrl);
-					System.out.println("webPage.attr: "+webPage.attr("abs:href"));
-					PageNode childNode = new PageNode();
-					if(StringUtils.isNotBlank(absUrl)){
+					System.out.println("webPage.attr: " + webPage.attr("abs:href"));
+					if (StringUtils.isNotBlank(absUrl) && !isUrlVisited(absUrl)) {
+						PageNode childNode = new PageNode();
 						Document childDoc = Jsoup.connect(absUrl).get();
 						childNode.setUrl(absUrl);
 						childNode.setTitle(childDoc.title());
 						visitedUrls.add(absUrl);
-						childNodes.add(childNode);
-						parentNode.setNodes(childNodes);
-						buildUrlPageTree(absUrl, depth);
+						childNodes.add(childNode);						
+						//buildUrlPageTree(absUrl, depth);
 					}
-				}	
-				
-				urlTree.add(parentNode);
-				
+				}
+				parentNode.setNodes(childNodes);
+				//urlTree.add(parentNode);
+
 			}
 			
 		} catch (IOException ie) {
@@ -74,7 +94,7 @@ public class UrlTrackingService implements UrlTrackingServiceIF {
 			ie.printStackTrace();
 		}
 		
-		return urlTree;
+		return parentNode;
 	}
 
 	private boolean isUrlVisited(String url) {
